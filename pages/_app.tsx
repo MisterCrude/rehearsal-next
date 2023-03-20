8305f7f2f4d1ccd3cd3ac32f01e7a83535917822
MisterCrude <andrzej.pluszcz@gmail.com>
2023-03-20 18:25:39 +0100

    feat: add auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps as Props } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface AppProps extends Props {
+  Component: Props["Component"];
+  pageProps: { session: Session; pageProps: Props["pageProps"] };
+}
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
