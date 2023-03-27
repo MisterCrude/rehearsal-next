ea83b540c49f41b44add91ae980b0ceda9b9dc19
MisterCrude <andrzej.pluszcz@gmail.com>
2023-03-27 15:03:36 +0200

    feat: add filters

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Filters from "@/components/Filters";
+import Filters, { ALL_INDEX } from "@/components/Filters";
 import Layout from "@/components/Layout";
 import StudioCard from "@/components/StudioCard";
 import { getDistricts } from "@/contentful/district";
@@ -6,16 +6,32 @@ import { getServices } from "@/contentful/service";
 import { getStudios } from "@/contentful/studio";
 import Stack from "@mui/material/Stack";
 import { InferGetServerSidePropsType } from "next";
+import { useState } from "react";
 
 export default function Home({
   studios,
   districts,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [filteredStudios, setFilteredStudios] = useState(studios);
+
+  const handleFilterChange = (selectedDistricts: string[]) => {
+    if (selectedDistricts.includes(ALL_INDEX)) {
+      return setFilteredStudios(studios);
+    }
+
+    const selected = studios.filter(({ district }) =>
+      selectedDistricts.includes(district.id)
+    );
+
+    return setFilteredStudios(selected);
+  };
+
   return (
     <Layout>
-      <Filters districts={districts} onChange={console.log} />
+      <Filters districts={districts} onChange={handleFilterChange} />
+
       <Stack gap={4}>
-        {studios.map((studio) => (
+        {filteredStudios.map((studio) => (
           <StudioCard key={studio.id} studio={studio} />
         ))}
       </Stack>
