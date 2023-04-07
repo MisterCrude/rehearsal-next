e235d3c1a64858091dde292fd3d8e226174811b1
MisterCrude <andrzej.pluszcz@gmail.com>
2023-04-07 16:03:00 +0200

    refactor: remove DistrictChip

--- components/Filters/Filters.tsx
+++ components/Filters/Filters.tsx
@@ -1,10 +1,7 @@
 import { District } from "@/resources/dto/studio";
-import DomainIcon from "@mui/icons-material/Domain";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import { useState } from "react";
-import DistrictChip from "./DistrictChip";
-import { setSelectedDistricts } from "./utils";
 
 export const ALL_INDEX = "all";
 
@@ -16,38 +13,11 @@ interface FiltersProps {
 export default function Filters({ districts, onChange }: FiltersProps) {
   const [selectedFilters, setSelectedFilters] = useState<string[]>([ALL_INDEX]);
 
-  const handleSelect = (districtId: string) => {
-    setSelectedFilters((prev) => {
-      const selected = setSelectedDistricts({
-        prev,
-        selected: districtId,
-        districtsLength: districts.length,
-        allIndex: ALL_INDEX,
-      });
-
-      onChange(selected);
-      return selected;
-    });
-  };
+  const handleSelect = (districtId: string) => {};
 
   return (
     <Box sx={{ overflowX: "auto" }}>
-      <Stack spacing={1.5} direction="row" sx={{ marginBottom: 2 }}>
-        <DistrictChip
-          onSelect={() => handleSelect(ALL_INDEX)}
-          name="Wszystkie"
-          selected={selectedFilters.includes(ALL_INDEX)}
-        />
-        {districts.map((district) => (
-          <DistrictChip
-            icon={<DomainIcon />}
-            onSelect={() => handleSelect(district.id)}
-            name={district.name}
-            selected={selectedFilters.includes(district.id)}
-            key={district.id}
-          />
-        ))}
-      </Stack>
+      <Stack spacing={1.5} direction="row" sx={{ marginBottom: 2 }}></Stack>
     </Box>
   );
 }
