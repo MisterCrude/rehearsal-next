25fe3bc9e0ad92cecd95f6ec04726d1b936a39e4
MisterCrude <andrzej.pluszcz@gmail.com>
2023-07-04 07:09:17 +0200

    refactor: change brand name

--- constants.ts
+++ constants.ts
@@ -1,3 +1,5 @@
-export const BRAND_NAME = "proba.pl";
-export const BRAND_URL = "https://proba.pl";
+export const BRAND_NAME = "musicspace.pl";
+export const BRAND_URL = "http://www.musicspace.pl";
+export const BRAND_EMAIL = "biuro@probmusicspacea.pl";
+
 export const COOKIE_ACCEPTED = "cookieAccepted";
